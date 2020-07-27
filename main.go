8e5d050e2c64927140ac9c88464d5161772fff77
David Salgado <david@digitalronin.com>
2020-07-27 11:53:17 +0100

    Set version to 0.0.4

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	rd := releaseData{
 		RepoName:       "self-updating-golang-binary",
 		Owner:          "digitalronin",
-		CurrentVersion: "0.0.2",
+		CurrentVersion: "0.0.4",
 	}
 
 	_, latest := rd.isLatestVersion()
